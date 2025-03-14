65c5d33db14b96301b3e8a8baa66d922eb9ab91c
Matyapiro31 <matyapiro31@gmail.com>
2025-03-14 14:21:40 +0000

    Remove NewRelic.

--- Rainier.NativeOmukadeConnector/Patches/NoTelemetryPatches.cs
+++ Rainier.NativeOmukadeConnector/Patches/NoTelemetryPatches.cs
@@ -17,12 +17,15 @@
 **************************************************************************/
 
 #nullable disable
-
+ 
 using HarmonyLib;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using TPCI.Commands;
 using TPCI.NetworkSystem;
@@ -53,64 +56,5 @@ namespace Rainier.NativeOmukadeConnector.Patches
         [HarmonyPatch(nameof(TelemetryManager.AddToQueue))]
         [HarmonyPrefix]
         static bool SilenceTpciTelemetry() => false;
-    }
-
-    [HarmonyPatch(typeof(InsightsAPI))]
-    static class NoNewRelic
-    {
-        [HarmonyPatch("SendPostQueueYield_Impl")]
-        [HarmonyPrefix]
-        static bool SendPostQueueYield1(ref IEnumerator __result)
-        {
-            __result = FakeEnumerator.INSTANCE;
-            return false;
-        }
-
-        [HarmonyPatch(nameof(InsightsAPI.SendPostQueueYield))]
-        [HarmonyPrefix]
-        static bool SendPostQueueYield2(ref IEnumerator __result)
-        {
-            __result = FakeEnumerator.INSTANCE;
-            return false;
-        }
-
-        [HarmonyPatch(nameof(InsightsAPI.PostYield), typeof(IInsightsEvent), typeof(IParamCommand<RestWorkerBase>), typeof(PostOptions))]
-        [HarmonyPatch(nameof(InsightsAPI.PostYield), typeof(List<IInsightsEvent>), typeof(IParamCommand<RestWorkerBase>), typeof(PostOptions))]
-        [HarmonyPrefix]
-        static bool SendPostQueueYield3(ref IEnumerator __result)
-        {
-            __result = FakeEnumerator.INSTANCE;
-            return false;
-        }
-
-        [HarmonyPatch(nameof(InsightsAPI.PostYield_Impl))]
-        [HarmonyPrefix]
-        static bool SendPostQueueYield4(ref IEnumerator __result)
-        {
-            __result = FakeEnumerator.INSTANCE;
-            return false;
-        }
-
-        [HarmonyPatch(nameof(InsightsAPI.Post))]
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> DummyOutMethod1(IEnumerable<CodeInstruction> instructions)
-        {
-            yield return new CodeInstruction(OpCodes.Ret);
-        }
-
-        [HarmonyPatch("ImmediatelyDispatchIfFull")]
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> DummyOutMethod2(IEnumerable<CodeInstruction> instructions)
-        {
-            yield return new CodeInstruction(OpCodes.Ret);
-        }
-
-        [HarmonyPatch(nameof(InsightsAPI.AddToPostQueue))]
-        [HarmonyPrefix]
-        static bool AddToPostQueue(InsightsAPI __instance, ref InsightsAPI __result)
-        {
-            __result = __instance;
-            return false;
-        }
     }
 }
